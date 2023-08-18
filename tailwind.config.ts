b81d4209ebce3756bb53388e04e6bfce9e5afe3d
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-08-19 00:04:26 +0100

    initial files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,20 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      'primary': '#322653',
+      'primary-active': '#f4f2f9',
+      'secondary': '#8062D6',
+      'tertiary': '#9288F8',
+      'accent': '#FFD2D7',
+      'white': '#fff',
+      'cool-grey': '#9699AB',
+      'light-grey': '#D5D8E5',
+      'bronze': '#a17652',
+      'silver': '#D6D6D6',
+      'gold': '#fcb434',
+      'error': '#cc0000'
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
