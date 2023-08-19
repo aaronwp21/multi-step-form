8f5e16b9907dca365c307fcf2fdc769e570a4195
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-08-19 17:38:01 +0100

    mobile step counter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,8 @@ export default function Home() {
   const { pageNum, confirmed, onUpdatePageNum, onUpdateConfirmed } = useContext(FormContext);
 
   return (
-    <div className="h-[100vh] grid grid-rows-[minmax(175px,2fr),minmax(450px,3fr),75px]">
-      <ol className="row-start-1 col-start-1 bg-secondary flex justify-center gap-4 pt-12">
+    <div className="h-[100vh] grid grid-rows-[minmax(175px,2fr),minmax(450px,3fr),75px] sm:w-[80%] sm:py-[10%] sm:m-auto sm:grid-rows-1 sm:grid-cols-[1fr,2fr]">
+      <ol className="row-start-1 col-start-1 bg-secondary flex justify-center gap-4 pt-12 sm:flex-col sm:justify-start sm:pl-8 sm:gap-8">
         <li className="flex items-start gap-4">
           <p
             className={`${
@@ -18,7 +18,7 @@ export default function Home() {
           >
             1
           </p>
-          <div className="text-white hidden">
+          <div className="text-white hidden sm:inline">
             <p className="text-xs text-light-grey">STEP 1</p>
             <p className="text-md font-bold">SELECT PLAN</p>
           </div>
@@ -31,7 +31,7 @@ export default function Home() {
           >
             2
           </p>
-          <div className="text-white hidden">
+          <div className="text-white hidden sm:inline">
             <p className="text-xs text-light-grey">STEP 2</p>
             <p className="text-md font-bold">YOUR INFO</p>
           </div>
@@ -44,13 +44,13 @@ export default function Home() {
           >
             3
           </p>
-          <div className="text-white hidden">
+          <div className="text-white hidden sm:inline">
             <p className="text-xs text-light-grey">STEP 3</p>
             <p className="text-md font-bold">SUMMARY</p>
           </div>
         </li>
       </ol>
-      <div className="row-start-1 row-end-3 col-start-1 z-50 flex pt-24">
+      <div className="row-start-1 row-end-3 col-start-1 sm:col-start-2 z-50 flex pt-24">
         <div className="w-[90%] h-[95%] m-auto bg-white rounded-xl p-8 relative">
           {pageNum === 0 ? (
             <SelectPlan />
@@ -62,7 +62,7 @@ export default function Home() {
         </div>
       </div>
       <div className="bg-light-grey row-start-2 col-start-1"></div>
-      <div className="grid items-center">
+      <div className="grid items-center bg-white">
         {pageNum === 0 ? (
           <div className="justify-self-end pr-[5%]">
             <button
